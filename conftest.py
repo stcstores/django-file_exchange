b0d7d85f9cbf0606ae42300f6ad41c31b5d55015
Luke Shiner <luke@lukeshiner.com>
2022-08-04 14:01:56 +0100

    Remove django_celery_results

--- conftest.py
+++ conftest.py
@@ -7,8 +7,5 @@ MEDIA_DIR = tempfile.TemporaryDirectory()
 pytest_plugins = ("celery.contrib.pytest",)
 pytest_plugins = configure_djangoapp_plugin(
     {"MEDIA_ROOT": tempfile.mkdtemp(), "DEBUG": True},
-    extend_INSTALLED_APPS=[
-        "django_celery_results",
-    ],
     migrate=True,
 )
